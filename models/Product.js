ecbb48b5a10b8d3648dcba8938ba6e50c8d32f31
Odis Clemons <odis.clemons@gmail.com>
2022-06-02 21:45:44 -0400

    fixed product model fixed seed index.js

--- models/Product.js
+++ models/Product.js
@@ -39,7 +39,7 @@ Product.init(
       type: DataTypes.INTEGER,
       references: {
         model: "category",
-        key: "category_id",
+        key: "id",
       },
     },
   },
--- seeds/index.js
+++ seeds/index.js
@@ -1,25 +1,27 @@
-const seedCategories = require('./category-seeds');
-const seedProducts = require('./product-seeds');
-const seedTags = require('./tag-seeds');
-const seedProductTags = require('./product-tag-seeds');
+require("dotenv").config();
+const seedCategories = require("./category-seeds");
+const seedProducts = require("./product-seeds");
+const seedTags = require("./tag-seeds");
+const seedProductTags = require("./product-tag-seeds");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
 const seedAll = async () => {
   await sequelize.sync({ force: true });
-  console.log('\n----- DATABASE SYNCED -----\n');
+  console.log("\n----- DATABASE SYNCED -----\n");
   await seedCategories();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+  console.log("\n----- CATEGORIES SEEDED -----\n");
 
   await seedProducts();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+  console.log("\n----- PRODUCTS SEEDED -----\n");
 
   await seedTags();
-  console.log('\n----- TAGS SEEDED -----\n');
+  console.log("\n----- TAGS SEEDED -----\n");
 
   await seedProductTags();
-  console.log('\n----- PRODUCT TAGS SEEDED -----\n');
+  console.log("\n----- PRODUCT TAGS SEEDED -----\n");
 
+  console.log(process.env.DB_NAME);
   process.exit(0);
 };
 
